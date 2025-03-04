66bfeb840e902e801e4cd0ecbc37a4df7cd42e3e
Hasan Halici <huseyinhalici@outlook.com>
2025-03-04 22:13:11 +0300

    TimeDurationWithUiAdded

--- Assets/_MyGame/Scripts/TimeChangeManager.cs
+++ Assets/_MyGame/Scripts/TimeChangeManager.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Mime;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
+// using UnityEngine.UIElements;
 
 public class TimeChangeManager : MonoBehaviour
 {
@@ -24,6 +26,10 @@ public class TimeChangeManager : MonoBehaviour
 
     public Image coolDownFıllImage;
     public float coolDownTime = 5;
+
+    public Image ProgressBarTime;
+    public GameObject TimerProgressBar;
+    public float ProgressDuration = 15f;
     
     private void Awake()
     {
@@ -79,12 +85,13 @@ public class TimeChangeManager : MonoBehaviour
     public void TimeChangeWar()
     {
             print("War");
+            TimerProgressBar.SetActive(false);
             TimeState = false;
             WarObjects.SetActive(true);
             PeaceObjects.SetActive(false);
             GlobalVolume.profile.TryGet(out colorAdjustments);
             colorAdjustments.saturation.value = -100; // Siyah-beyaz yap
-
+            Debug.Log("War Çağırıldı");
             ChangeSkıllAndCooldown(true);
             
     }
@@ -92,6 +99,10 @@ public class TimeChangeManager : MonoBehaviour
     public void TimeChangePeace()
     {
             print("Peace");
+            TimerProgressBar.SetActive(true);
+            ProgressBarTime.fillAmount = 1f;
+            StartCoroutine(RopeProgressDecrase());
+
             TimeState = true;
             WarObjects.SetActive(false);
             PeaceObjects.SetActive(true);
@@ -103,6 +114,8 @@ public class TimeChangeManager : MonoBehaviour
     }
 
     private void ChangeSkıllAndCooldown(bool WarState)
+
+
     {
         coolDownIsReady = false;
         coolDownFıllImage.fillAmount = 1;
@@ -117,6 +130,34 @@ public class TimeChangeManager : MonoBehaviour
             PeaceImage.SetActive(false);
             WarImage.SetActive(true);
         }
+    }
+
+
+    private void StartRopeTimer()
+    {
+        StopAllCoroutines();
+        ProgressBarTime.fillAmount = 1f;
+        StartCoroutine(RopeProgressDecrase());
+    }
+
+    private IEnumerator RopeProgressDecrase()
+    {
+        float elapsedTime = 0f;
+        float startValue = 1f; // Başlangıç değeri (dolu)
+        float endValue = 0f;   // Bitiş değeri (boş)
+        Debug.Log("PROGRESS DURATİON :"+ ProgressDuration);
+        while (elapsedTime < ProgressDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            Debug.Log(elapsedTime);
+            ProgressBarTime.fillAmount = Mathf.Lerp(startValue, endValue, elapsedTime / ProgressDuration);
+            yield return null; // Bir sonraki frame'e geç
+        }
+
+        ProgressBarTime.fillAmount = 0f; // Son değeri kesin olarak sıfırla
+        Debug.Log("War Çağırılıyor");
+
+        TimeChangeWar();
     }
 
 }
