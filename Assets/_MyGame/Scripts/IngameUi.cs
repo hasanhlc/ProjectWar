ba4f3b95ae8e1b87eeb94057b5b62573b4599797
Hasan Halici <huseyinhalici@outlook.com>
2025-04-17 22:38:04 +0300

    ingameuiAdded

--- Assets/_MyGame/Scripts/IngameUi.cs
+++ Assets/_MyGame/Scripts/IngameUi.cs
@@ -1,12 +1,20 @@
+using Unity.VisualScripting;
 using UnityEditor.Rendering;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class IngameUi : MonoBehaviour
 {
 
     GeneralGame ınputActions;
 
+    private PlayerInput playerInput;
+    private bool isPaused = false;
+
     public GameObject IngameUI;
+    public GameObject PlayerController;
+
 
 
 
@@ -16,19 +24,76 @@ public class IngameUi : MonoBehaviour
         ınputActions = new GeneralGame();
         ınputActions.UIIngame.Enable();
     }
+
+    void Start()
+    {
+        playerInput = PlayerController.GetComponent<PlayerInput>();
+        ResumeGame();
+    }
     void Update()
     {
         if (ınputActions.UIIngame.OpenUIIngame.WasPressedThisFrame())
         {
             if (IngameUI.activeSelf == false)
             {
-                IngameUI.SetActive(true);
+                // Oyun içi menü açık oyun durdu
+                PauseGame();
             }
             else
             {
-                IngameUI.SetActive(false);
+                // Oyun içi menü kapalı oyun devam ediyor
+                ResumeGame();
             }
         }
-
     }
+
+    public void ResumeGame()
+    {
+        // Oyun içi menü kapalı oyun devam ediyor
+        IngameUI.SetActive(false);
+        playerInput.enabled = true;
+        isPaused = false;
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void PauseGame()
+    {
+        // Oyun içi menü açık oyun durdu
+        IngameUI.SetActive(true);
+        playerInput.enabled = false;
+        isPaused = true;
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void OnApplicationFocus(bool focus)
+    {
+        if (isPaused)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Debug.Log("Cursor kilenmiyor");
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            Debug.Log("Cursor kilitleniyor");
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Çıkış yapılıyor...");
+    }
+
+    public void ExitToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
 }
